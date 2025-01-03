e70676ef4b204344c36f5a4381b7005f967af598
Ronny Perez <ronnypdev@gmail.com>
2025-01-03 10:21:22 -0500

    fix font face bug

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -94,7 +94,9 @@ function RecommendedProducts({
 }) {
   return (
     <div className="recommended-products">
-      <h2 className="text-brand-gold font-playfair font-medium">Recommended Products</h2>
+      <h2 className="text-brand-gold font-playfair font-bold">
+        Recommended Products
+      </h2>
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={products}>
           {(response) => (
